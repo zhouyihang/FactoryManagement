ca5031a394bb11a69935de08889865f41f0b1a25
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:23:47 +0800

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,11 +3,12 @@ insert into order_table (order_id, actual_deliver_date, expect_deliver_date, ord
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000002, '2017-04-01', '2017-08-01', 'wip');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000003, '2017-06-01', '2017-08-01', 'wip');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000004, '2017-08-01', '2017-08-01', 'new');
+insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000005, '2018-01-01', '2018-01-01', 'new');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000006, '2018-01-01', '2018-01-01', 'new');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000007, '2018-03-01', '2018-01-01', 'new');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000008, '2018-05-01', '2018-07-01', 'new');
 insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000009, '2018-07-01', '2018-09-01', 'completed');
-insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000010, '2018-09-01', '2018-09-01', 'completed);
+insert into order_table (order_id, actual_deliver_date, expect_deliver_date, order_status) values (1000000010, '2018-09-01', '2018-09-01', 'completed');
 
 insert into product_table (product_id, product_name, product_unit, speed_quantity, speed_second) values (3000000000, 'prod 1', 'unit 1', 1, 100)
 insert into product_table (product_id, product_name, product_unit, speed_quantity, speed_second) values (3000000001, 'prod 2', 'unit 2', 1, 100)
@@ -31,6 +32,7 @@ insert into source_table (source_id, price, provider_name, source_name, source_u
 insert into source_table (source_id, price, provider_name, source_name, source_unit) values (5000000007, 6.02, 'hkg', 'hahah', 'kg')
 insert into source_table (source_id, price, provider_name, source_name, source_unit) values (5000000008, 6.02, 'hkg', 'hahah', 'kg')
 insert into source_table (source_id, price, provider_name, source_name, source_unit) values (5000000009, 6.02, 'hkg', 'hahah', 'kg')
+insert into source_table (source_id, price, provider_name, source_name, source_unit) values (5000000010, 6.02, 'hkg', 'hahah', 'kg')
 
 insert into work_table (work_id, actual_deliver_date, expect_deliver_date, order_order_id, product_product_id, plan_deliver_date, work_status, quantity) values (2000000000, '2017-01-01', '2017-01-01', 1000000000, 3000000000, '2017-01-01', 'New', 10000);
 insert into work_table (work_id, actual_deliver_date, expect_deliver_date, order_order_id, product_product_id, plan_deliver_date, work_status, quantity) values (2000000001, '2017-01-01', '2017-01-01', 1000000001, 3000000001, '2017-01-01', 'New', 10000);
