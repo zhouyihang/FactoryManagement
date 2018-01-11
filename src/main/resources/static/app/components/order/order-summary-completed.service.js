f294fb882b513debe0e264dd58d3378e98e99259
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:36:46 +0800

    Create order-summary-completed.service.js

---
+++ src/main/resources/static/app/components/order/order-summary-completed.service.js
@@ -0,0 +1,43 @@
+angular.module('appFactory').service(
+    "orderSummaryCompletedService",
+    function ($http, $q) {
+        // API
+        return ({
+            getOrderSummary: getOrderSummary
+        });
+
+        // PUBLIC METHODS.
+
+        // Get order list
+        function getOrderSummary() {
+            console.log();
+            var request = $http({
+                method: "get",
+                url: "order/summary/completed",
+                params: {
+                    action: "get"
+                }
+            });
+            return ( request.then(handleSuccess, handleError) );
+        }
+
+        // PRIVATE METHODS.
+
+        // Transform the error response, unwrapping the application dta from
+        // the API response payload.
+        function handleError(response) {
+            if (
+                !angular.isObject(response.data) || !response.data.message
+            ) {
+                return ( $q.reject("An unknown error occurred.") );
+            }
+            return ( $q.reject(response.data.message) );
+        }
+
+        // I transform the successful response, unwrapping the application data
+        // from the API response payload.
+        function handleSuccess(response) {
+            return ( response.data );
+        }
+    }
+);
