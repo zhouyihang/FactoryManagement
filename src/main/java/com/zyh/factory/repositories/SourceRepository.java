6d50b835763af619c6df7120aac673ffe42b75b3
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:30:37 +0800

    Update SourceRepository.java

--- src/main/java/com/zyh/factory/repositories/SourceRepository.java
+++ src/main/java/com/zyh/factory/repositories/SourceRepository.java
@@ -1,8 +1,13 @@
 package com.zyh.factory.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.zyh.factory.entity.SourceEntity;
 
 public interface SourceRepository extends CrudRepository<SourceEntity, Long> {
 
+	@Query("select new map(count(v) as count, v.sourceStatus as label) from SourceEntity v group by v.sourceStatus")
+	public List<?> countGroupBySourceStatus();
 }
