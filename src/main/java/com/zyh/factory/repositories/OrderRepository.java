590fe0fca058728fb8473fc59f022d1479e48bf9
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:32:13 +0800

    Update OrderRepository.java

--- src/main/java/com/zyh/factory/repositories/OrderRepository.java
+++ src/main/java/com/zyh/factory/repositories/OrderRepository.java
@@ -18,11 +18,17 @@ public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
 //    " from OrderEntity " +
 //    " group by extract(year from expectDeliverDate), extract(month from expectDeliverDate) " +
 //    " order by yr, mon")
-	@Query("select new map(to_char(expectDeliverDate, 'YYYY-MM') as label, " +
-		    " count(*) as count) " +
+	@Query("select new map(to_char(expectDeliverDate, 'YYYY-MM') as label, count(*) as count) " +
 		    " from OrderEntity " +
+			" where orderStatus != 'completed' " +
 		    " group by to_char(expectDeliverDate, 'YYYY-MM') " +
 		    " order by label")
-	public List<?> countGroupByMonth();
+	public List<?> countActiveGroupByMonth();
+	@Query("select new map(to_char(expectDeliverDate, 'YYYY-MM') as label, count(*) as count) " +
+		    " from OrderEntity " +
+			" where orderStatus = 'completed' " +
+		    " group by to_char(expectDeliverDate, 'YYYY-MM') " +
+		    " order by label")
+	public List<?> countCompletedGroupByMonth();
 
 }
