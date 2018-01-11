a6e3d69f4ca7e2e62358a9b572a0254691dbea6b
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:29:20 +0800

    Update MachineRepository.java

--- src/main/java/com/zyh/factory/repositories/MachineRepository.java
+++ src/main/java/com/zyh/factory/repositories/MachineRepository.java
@@ -1,9 +1,15 @@
 package com.zyh.factory.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.zyh.factory.entity.MachineEntity;
 
-public interface MachineRepository extends CrudRepository<MachineEntity, String> {
+public interface MachineRepository extends CrudRepository<MachineEntity, Long> {
 
+	@Query("select new map(count(v) as count, v.machineStatus as label) from MachineEntity v group by v.machineStatus")
+	public List<?> countGroupByMachineStatus();
+	
 }
