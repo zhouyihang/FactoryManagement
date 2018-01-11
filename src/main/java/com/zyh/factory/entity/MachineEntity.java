638e16f68cd5b19cd3831d000391b7329ce3ec67
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:28:33 +0800

    Update MachineEntity.java

--- src/main/java/com/zyh/factory/entity/MachineEntity.java
+++ src/main/java/com/zyh/factory/entity/MachineEntity.java
@@ -15,13 +15,53 @@ public class MachineEntity {
 
     @Id
     @GeneratedValue
-    private String machineId;
+    private Long machineId;
+    @Column
+    private String machineStatus;
     @Column
     private String machineName;
     @Column
     private String machineModel;
     @Column
-    private int machineSpeed; //default 1.
+    private Integer machineSpeed; //default 1.
     @Embedded
     private List<MachineWorkHourEntity> machineWorkhour;
+	public Long getMachineId() {
+		return machineId;
+	}
+	public void setMachineId(Long machineId) {
+		this.machineId = machineId;
+	}
+	public String getMachineStatus() {
+		return machineStatus;
+	}
+	public void setMachineStatus(String machineStatus) {
+		this.machineStatus = machineStatus;
+	}
+	public String getMachineName() {
+		return machineName;
+	}
+	public void setMachineName(String machineName) {
+		this.machineName = machineName;
+	}
+	public String getMachineModel() {
+		return machineModel;
+	}
+	public void setMachineModel(String machineModel) {
+		this.machineModel = machineModel;
+	}
+	public Integer getMachineSpeed() {
+		return machineSpeed;
+	}
+	public void setMachineSpeed(Integer machineSpeed) {
+		this.machineSpeed = machineSpeed;
+	}
+	public List<MachineWorkHourEntity> getMachineWorkhour() {
+		return machineWorkhour;
+	}
+	public void setMachineWorkhour(List<MachineWorkHourEntity> machineWorkhour) {
+		this.machineWorkhour = machineWorkhour;
+	}
+    
+    
 }
