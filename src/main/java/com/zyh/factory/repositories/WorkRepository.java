a8ca88f78107fef6744bd7d47fa9aebf04b8e302
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:31:08 +0800

    Update WorkRepository.java

--- src/main/java/com/zyh/factory/repositories/WorkRepository.java
+++ src/main/java/com/zyh/factory/repositories/WorkRepository.java
@@ -1,8 +1,14 @@
 package com.zyh.factory.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.zyh.factory.entity.WorkEntity;
 
 public interface WorkRepository extends CrudRepository<WorkEntity, Long> {
 
+	@Query("select new map(count(v) as count, v.workStatus as label) from WorkEntity v group by v.workStatus")
+	public List<?> countGroupByWorkStatus();
+	
 }
