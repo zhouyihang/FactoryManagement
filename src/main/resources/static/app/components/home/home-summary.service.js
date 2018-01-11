a0f4f6d96e9dbd0effaaf6194f9332416de21b6a
zhou yihang <zhouyihangok@163.com>
2018-01-11 14:23:18 +0800

    Create home-summary.service.js

---
+++ src/main/resources/static/app/components/home/home-summary.service.js
@@ -0,0 +1,43 @@
+angular.module('appFactory').service(
+    "homeSummaryService",
+    function ($http, $q) {
+        // API
+        return ({
+            getHomeSummary: getHomeSummary
+        });
+
+        // PUBLIC METHODS.
+
+        // Get home list
+        function getHomeSummary() {
+            console.log();
+            var request = $http({
+                method: "get",
+                url: "summary",
+                params: {
+                    action: "get"
+                }
+            });
+            return ( request.then(handleSuccess, handleError) );
+        }
+
+        // PRIVATE METHODS.
+
+        // Transform the error response, unwrapping the application dta from
+        // the API response payload.
+        function handleError(response) {
+            if (
+                !angular.isObject(response.data) || !response.data.message
+            ) {
+                return ( $q.reject("An unknown error occurred.") );
+            }
+            return ( $q.reject(response.data.message) );
+        }
+
+        // I transform the successful response, unwrapping the application data
+        // from the API response payload.
+        function handleSuccess(response) {
+            return ( response.data );
+        }
+    }
+);
